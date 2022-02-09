13aa8227a985debbcac60623f4c8f204d4964271
Jimmy Tang <jimmy.tang.360@gmail.com>
2022-02-09 02:25:17 +0000

    BootcampX Queries II

---
+++ 2_queries_joins/cohort_total_duration.sql
@@ -0,0 +1,9 @@
+SELECT sum(duration) as total_duration
+
+FROM assignment_submissions 
+
+JOIN students ON students.id = student_id
+
+JOIN cohorts ON cohort_id = cohorts.id
+
+WHERE cohorts.name = 'FEB12';
